945a4887592e7d7901224e6a918e75d9c015a19c
Curtis Timson <curt@live.co.uk>
2019-09-09 21:57:24 +0100

    Add is-preload class (#5)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,9 @@
  */
 
 // You can delete this file if you're not using it
+exports.onClientEntry = () => {
+  if ('addEventListener' in window) {
+    window.addEventListener('load', function() { document.body.className = document.body.className.replace(/\bis-preload\b/, ''); });
+    document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
+	}
+}
