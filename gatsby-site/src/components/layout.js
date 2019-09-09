54ce359aa56954fb0d4b906c616e7bb3eec4564a
Curtis Timson <curt@live.co.uk>
2019-09-09 19:51:47 +0100

    Update footer

--- gatsby-site/src/components/layout.js
+++ gatsby-site/src/components/layout.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import "./layout.css"
+import "../assets/css/main.css";
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -24,19 +24,12 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
+      <div>
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        <footer id="footer">
+          <ul class="copyright">
+            <li>&copy; Jane Doe</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+          </ul>
         </footer>
       </div>
     </>
