b7b573b9639fe28cc7894dcf3b1f94cde8f15023
Curtis Timson <curt@live.co.uk>
2019-09-10 19:45:50 +0100

    Get title and subtitle via graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,20 +7,33 @@ import SEO from "../components/seo"
 import avatarImg from '../images/avatar.jpg';
 import SocialLinks from '../components/socialLinks';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <section id="main">
-      <header>
-        <span class="avatar"><img src={avatarImg} alt="" /></span>
-        <h1>PDTD</h1>
-        <p>Senior Psychonautics Engineer</p>
-      </header>
-      <footer>
-        <SocialLinks/>
-      </footer>
-    </section>
-  </Layout>
-)
+const IndexPage = () => {
+
+  const { metaJson } = useStaticQuery(
+    graphql`
+      query {
+        metaJson {
+          subtitle
+          title
+        }
+      }
+    `);
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <section id="main">
+        <header>
+          <span class="avatar"><img src={avatarImg} alt="" /></span>
+          <h1>{metaJson.title}</h1>
+          <p>{metaJson.subtitle}</p>
+        </header>
+        <footer>
+          <SocialLinks/>
+        </footer>
+      </section>
+    </Layout>
+  )
+}
 
 export default IndexPage
