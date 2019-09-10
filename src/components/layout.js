2c50a065211648f8fee3db1557fef99096b2ddd4
Curtis Timson <curt@live.co.uk>
2019-09-10 19:48:53 +0100

    Use graphql title in footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,28 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
 
 import '../assets/css/gatsby-specific.css';
 import "../assets/css/main.css";
 
 const Layout = ({ children }) => {
+
+  const { metaJson } = useStaticQuery(
+    graphql`
+      query {
+        metaJson {
+          title
+        }
+      }
+    `);
+
   return (
     <>
       <div id="wrapper">
         {children}
         <footer id="footer">
           <ul class="copyright">
-            <li>&copy; PDTD</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+            <li>&copy; {metaJson.title}</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
           </ul>
         </footer>
       </div>
