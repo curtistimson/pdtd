84baff360e12833fcb17ad6d399bf7909ea1aaa1
Curtis Timson <curt@live.co.uk>
2019-09-11 13:28:30 +0100

    Add google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,8 +30,11 @@ module.exports = {
         path: `${__dirname}/src/data`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-147663435-1",
+      },
+    },
   ],
 }
